af4047999fbc42a7cc776940ce2c893a7fa2a5ad
Adrian Evangelista <adriane.macer@gmail.com>
2019-09-28 22:21:42 +0800

    Added Files

--- lib/blocs/billing_bloc/billing_bloc.dart
+++ lib/blocs/billing_bloc/billing_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:gov/blocs/billing_event.dart';
 import 'package:gov/blocs/billing_state.dart';
+import 'package:gov/models/bill.dart';
 import 'package:gov/services/data_service.dart';
 import 'package:gov/services/service_locator.dart';
 
@@ -9,12 +10,24 @@ class BillingBloc extends Bloc<BillingEvent, BillingState> {
   @override
   BillingState get initialState => BillingUninitialized();
 
+  List<Bill> bills = [];
+
   @override
   Stream<BillingState> mapEventToState(BillingEvent event) async* {
-    if(event is FetchBilling){
+    if (event is FetchBilling) {
       yield BillingLoading();
-      final bills = await locator<DataService>().getData();
+      try {
+        bills = await locator<DataService>().getData();
+      } catch (e) {
+        print(e);
+        yield BillingError(Exception("Error in fetching bills"));
+      }
+      if (bills.isEmpty) {
+        yield NoBillingLoaded();
+        return;
+      }
       yield BillingLoaded(bills);
+      return;
     }
   }
 }
